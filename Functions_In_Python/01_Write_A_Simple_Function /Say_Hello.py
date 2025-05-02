52a46eb8109d0912374bca3cb68c72a1e4363c6a
alrasap <931006@pdsb.net>
2025-05-02 13:10:40 -0400

    done

---
+++ Functions_In_Python/01_Write_A_Simple_Function /Say_Hello.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+# Name:        Say Hello (Say_Hello.py)
+# Purpose:     To demonstrate how to write and use a simple function in Python
+#              that prints a greeting message to the user.
+#
+# Author:      Rohan
+# Created:     Apr 29, 2025
+# Updated:     Apr 29, 2025
+# -----------------------------------------------------------------------------
+
+# This function is called 'say_hello'. It doesn't take any inputs.
+# When you call this function, it will print "Hello, world!" on the screen.
+def say_hello():
+    print("Hello, world!")
+
+
+# Call the function to see the message
+say_hello()
+
+# Ask the user if they want to run the function again
+while True:
+    answer = input("Would you like to say hello again? (yes/no): ").strip().lower()
+
+    if answer == "yes":
+        say_hello()
+    elif answer == "no":
+        print("Alright, goodbye! 👋")
+        break
+    else:
+        print("Please enter 'yes' or 'no'.")
---
+++ Functions_In_Python/02_Add_Two_Numbers /README.md
@@ -0,0 +1,31 @@
+## Add Two Numbers Program
+
+### Purpose:
+This Python program demonstrates how to use a simple function called `add_numbers()`  
+that takes in two numbers, adds them, and returns the result.  
+The user can keep entering numbers to add until they decide to stop.
+
+---
+
+### Created by:
+Rohan  
+Student Number: 931006  
+Date: Apr 29, 2025  
+
+---
+
+### Requirements:
+* Python 3.x
+
+---
+
+### Instructions:
+1. Run the program.
+2. You will be asked to enter two numbers.
+3. The program will add them together using a function and show you the result.
+4. You will be asked if you'd like to do it again.
+5. Type `yes` to add more numbers or `no` to exit the program.
+
+---
+
+### Pseudo Code:
